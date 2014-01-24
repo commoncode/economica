46fe21b067822919f50b4f3504ff1fa44a6f019a
Daryl Antony <daryl@commoncode.com.au>
2014-01-24 18:17:14 +1100

    Offer model draft notes // WIP // Exploring Aspect-Based classes.

--- commercia/offers/models.py
+++ commercia/offers/models.py
@@ -23,12 +23,16 @@ class Offer(EnabledMixin, StartEndMixin):
     a Sales Order or Subscriptions.
 
     The use of Offers opens the door to create binding Contracts, enforceable by 
-    Workflow or Finite State Machine for Goods & Services offered under any crazy
+    Workflow or Finite State Machines for Goods & Services offered under any crazy
     business model that the Enterprise Agent can imagine.
 
     OR
 
     An Offer has one or more Offer Conditions, which may or may not cumulate
+
+    ## Attributes
+
+
 
     """
 
@@ -41,20 +45,9 @@ class Offer(EnabledMixin, StartEndMixin):
     platforms = models.ManyToManyField('platform.Platform')
 
 
-class RelatedOffer(PolyMorphicModel):
+class OfferAspect(PolyMorphicModel, DescriptionMixin, EnabledMixin, OrderingMixin, TitleMixin):
     """
-    A Related Offer is a sub offer placed atop of a Primary Offer.
-
-    A Related Offer can be offered in conjunction with a Primary Offer or as a Replacement
-
-    """
-
-    offer = models.ForeignKey('Offer')
-
-
-class OfferCondition(DescriptionMixin, EnabledMixin, OrderingMixin, StartEndMixin, TitleMixin):
-    """
-    Each Offer must specify one or more Conditions in which the Contract Offer
+    Each Offer must specify one or more Aspect Conditions in which the Contract Offer
     might be valid.
 
     This can include altering the Price and circumstances of the Contract.
@@ -63,20 +56,62 @@ class OfferCondition(DescriptionMixin, EnabledMixin, OrderingMixin, StartEndMixi
     Offered under.  When the Contract is finally instantiated as an Actual Contract,
     The Conditions in which it were offered under play a part in determining its Clauses
     and Parameters.
+
+    Attributes:
+
+    offer
+        fk
+    contract_price
+        set the price of the contract for this given aspect/offer/platform(s)
+    contract_discount
+        apply an optional discount
     """
     # title
     # short_title
     # description
     # order
-    # start
-    # end (optional)
     # enabled
 
     offer = models.ForeignKey('Offer')
 
+    contract_price = models.FloatField(blank=True)  # Need to confirm the best field type for prices.
+    contract_discount = models.FloatField(blank=True)
 
-    contract_price = models.FloatField()  # Need to confirm the best field type for prices.
-    contract_discount = models.FloatField()
+    accumulate_evaluation = models.BooleanField()
+    continue_evaluation = models.BooleanField()
 
 
 
+class OfferValidUntil(OfferAspect, StartEndMixin):
+    """
+    Here we add a Validity period for an Aspect Condition of the Offer.
+
+    This Aspect might be used to create a Flash Sale for the parent Offer
+    """
+    # start
+    # end (optional)
+    pass
+
+
+class OfferRelated(OfferAspect):
+    """
+    Here we might add Related Offers to the Primary Offer.  This might take the form
+    of Offering a closely Related Product to accompany the Primary Offer.
+
+    """
+    related_offer = models.ForeignKey(
+        'Offer',
+        related_name="%(app_label)s_%(class)s_related_offer")
+    related_contract_price = models.FloatField()
+    related_contract_discount = models.FloatField()
+
+
+class OfferIndividualAgent(OfferAspect):
+    """
+    Theoretically offer a unique set of Agents an Offer Aspect based on some kind of
+    activity.
+    
+    For example, if the Agent has had this Offer or a Related Offer in their Quote/Cart before
+    then perhaps offer them a better deal?
+    """
+    pass
