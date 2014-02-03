27bf5ef5174bee434471366e558321e503ad6eb6
Daryl Antony <daryl@commoncode.com.au>
2014-02-04 02:04:25 +1100

    WIP OfferAspects logic

--- commercia/offers/models.py
+++ commercia/offers/models.py
@@ -35,7 +35,24 @@ class Offer(EnabledMixin, StartEndMixin):
 
     ## Attributes
 
+    `contracts`
+        A combination of one or more Contracts offering Resources under the Conditions
+        specified by the Contract Clauses.
 
+        In this way we can effect concepts such as Bundling with a great deal of flexibility
+        for example:
+
+                Product A is offered under a typical RRP SalesOrder contract, and combined in the Offer
+                with Product B, offered under a Subscription Contract.
+
+                The product text might look like:
+
+                    + Purchase Product A one time, and recieve the associated refil Product B under a
+                    12 Month Subscription.
+
+    `platforms`
+        The Platforms aka sales channels underwhich the Contract or Contracts mix are Offered.  In this
+        way, Offers are reuseable.
 
     """
 
@@ -43,9 +60,11 @@ class Offer(EnabledMixin, StartEndMixin):
     # end (optional)
     # enabled
 
-    contract = models.ForeignKey('rea.Contract')
+    contracts = models.ManyToManyField('rea.Contract')
 
     platforms = models.ManyToManyField('platforms.Platform')
+
+    offer_price = models.FloatField(blank=True)  # Need to confirm the best field type for prices.
 
 
 class OfferAspect(REAModel, TextMixin, EnabledMixin, OrderingMixin, TitleMixin):
@@ -60,15 +79,20 @@ class OfferAspect(REAModel, TextMixin, EnabledMixin, OrderingMixin, TitleMixin):
     The Conditions in which it were offered under play a part in determining its Clauses
     and Parameters.
 
-    Attributes:
+    ## Attributes:
 
-    offer
-        fk
-    contract_price
-        set the price of the contract for this given aspect/offer/platform(s)
-    contract_discount
-        apply an optional discount
+    `offer`
+        the offer that this aspect should affect
+
+    `chain_evaluation`
+        offer aspects can be combined to create unique Promotion Offers of any type.  The
+        chain is broken when there is a sequence discontinuation is `chain_evaluation = True`
+
+    `override_evaluation`
+        the Offer Aspect or Offer Aspect Chain overrides all previous
+
     """
+
     # title
     # short_title
     # description
@@ -77,12 +101,17 @@ class OfferAspect(REAModel, TextMixin, EnabledMixin, OrderingMixin, TitleMixin):
 
     offer = models.ForeignKey('Offer')
 
-    contract_price = models.FloatField(blank=True)  # Need to confirm the best field type for prices.
-    contract_discount = models.FloatField(blank=True)
+    chain_evaluation = models.BooleanField()  # evaluate in combination with previous others / evaluate separately
+    stop_evaluating = models.BooleanField()  # stop evaluating all other aspects beyond this one.
+    override_evaluation = models.BooleanField()  # override all previous offer aspect chains
 
-    accumulate_evaluation = models.BooleanField()
-    continue_evaluation = models.BooleanField()
 
+class OfferDiscount(OfferAspect):
+    """
+    Offer a Discount in the form of a Percentage or Deduction
+    """
+    offer_discount = models.FloatField()
+    offer_discount_is_percentage = models.BooleanField(default=True)
 
 
 class OfferValidUntil(OfferAspect, StartEndMixin):
@@ -91,6 +120,7 @@ class OfferValidUntil(OfferAspect, StartEndMixin):
 
     This Aspect might be used to create a Flash Sale for the parent Offer
     """
+
     # start
     # end (optional)
     pass
@@ -101,12 +131,47 @@ class OfferRelated(OfferAspect):
     Here we might add Related Offers to the Primary Offer.  This might take the form
     of Offering a closely Related Product to accompany the Primary Offer.
 
+    Setting the `related_contract_price` to zero or the `related_contract_discount` to 100%
+    results in a Free Gift
     """
+
     related_offer = models.ForeignKey(
         'Offer',
         related_name="%(app_label)s_%(class)s_related_offer")
     related_contract_price = models.FloatField()
     related_contract_discount = models.FloatField()
+
+
+class OfferFreeGift(OfferAspect):
+    """
+    The existence of this Offer Aspect enables the combining with another Offer
+    for Free
+    """
+    free_offer = models.ForeignKey(
+        'Offer',
+        related_name="%(app_label)s_%(class)s_free_offer")
+
+
+class OfferFreeShipping(OfferAspect):
+    """
+    Offer Free Shipping
+    """
+    offer_free_shipping = models.BooleanField()
+
+
+class OfferNForOne(OfferAspect):
+    """
+    Offer a quantity of the same Contract for the same Product with Product Variants.
+
+    """
+
+    offer_quantity = models.IntegerField()  # 2 or more
+
+    # apply_to = ['category', 'product', 'variant',]  default 'product'
+
+    # XXX strategy to offer 'like' Products, i.e. offering Product Variants of the same Product.  Or
+    # open up to Products within the same Category, i.e. 't-shirts'
+    # validate on save offer_quantity
 
 
 class OfferIndividualAgent(OfferAspect):
@@ -116,5 +181,10 @@ class OfferIndividualAgent(OfferAspect):
 
     For example, if the Agent has had this Offer or a Related Offer in their Quote/Cart before
     then perhaps offer them a better deal?
+
+    Note: these are best created with an algorithm.  Rule-based Offer Aspects will be better applicable.
+
     """
-    pass
+
+    agents = models.ManyToManyField('rea.Agent')
+
