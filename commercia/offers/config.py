a5b028c8cc6abcc947398b1da2d870fe9758bcff
Daryl Antony <daryl@commoncode.com.au>
2014-02-22 22:58:20 +1100

    App Config

---
+++ commercia/offers/config.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.utils.importlib import import_module
+
+class OffersConfig(AppConfig):
+    name = 'commercia.offers'
+    verbose_name = "Offers"
+
+    def ready(self):
+        import_module('commercia.offers.collections')
