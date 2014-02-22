e88193c3a09a1ede825b8ff8cd3569f28a76bf52
Daryl Antony <daryl@commoncode.com.au>
2014-02-22 22:57:24 +1100

    adding/refining OfferResource Offer serialises and collections

--- commercia/offers/collections.py
+++ commercia/offers/collections.py
@@ -1,11 +1,13 @@
-from .models import Offer, OfferAspect
+from .models import (
+    Offer, OfferAspect, OfferResourceContract
+)
 
 from cqrs.mongo import mongodb, DRFDocumentCollection
 
 
 class OfferDocumentCollection(DRFDocumentCollection):
     """
-    A denormalized collection of `Contract`
+    A denormalized collection of `Offers`
     """
     name = 'offer'
     model = Offer
@@ -13,4 +15,27 @@ class OfferDocumentCollection(DRFDocumentCollection):
     name = 'economica__offers'
 
 
+class OfferResourceContractDocumentCollection(DRFDocumentCollection):
+    """
+    A denormalized collection of `Offers`
+    """
+    name = 'offerresourcecontract'
+    model = OfferResourceContract
+    serializer_class = 'commercia.offers.serializers.OfferResourceContractSerializer'
+    parent_collection = OfferDocumentCollection
+    name = 'economica__offers__resourcecontracts'
+
+
+class OfferAspectDocumentCollection(DRFDocumentCollection):
+    """
+    A denormalized collection of `Offer Aspects`
+    """
+    name = 'offeraspect'
+    model = OfferAspect
+    serializer_class = 'commercia.offers.serializers.OfferAspectSerializer'
+    name = 'economica__offers__offeraspects'
+
+
 mongodb.register(OfferDocumentCollection())
+mongodb.register(OfferResourceContractDocumentCollection())
+mongodb.register(OfferAspectDocumentCollection())
--- commercia/offers/serializers.py
+++ commercia/offers/serializers.py
@@ -1,31 +1,50 @@
 from cqrs.mongo import CQRSSerializer
 
-from .models import Offer
+from rest_framework import serializers
+
+from .models import (
+    Offer, OfferAspect, OfferResourceContract
+)
+
+
+class OfferResourceContractSerializer(CQRSSerializer):
+
+    class Meta:
+        model = OfferResourceContract
+        fields = (
+            'id', 
+            'contract', 
+            'resource', 
+            'quantity'
+        )
 
 
 class OfferSerializer(CQRSSerializer):
-    """
+    '''
     Serializer for the `SubscriptionContract` model
-    """
+    '''
 
-    # aspects = OfferAspectSerializer()
+    resource_contracts = OfferResourceContractSerializer(many=True)
 
     class Meta:
         model = Offer
         fields = (
-            "id", "title", "short_title"
+            'id', 
+            'title', 
+            'short_title',
+            'resource_contracts'
         )
 
 
 class OfferAspectSerializer(CQRSSerializer):
-    """
+    '''
     Serializer for Polymorphic Model OfferAspect.
-    """
+    '''
 
     class Meta:
-        model = 'offers.OfferAspect'
-
+        model = OfferAspect
         # dynamically add fields according to the downcast
         fields = (
-            "id", "title", "short_title"
+            'id', 'title', 'short_title'
         )
+
