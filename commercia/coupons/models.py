f1a16f156519bc3e15532eace31d1911f8e77b82
Daryl Antony <daryl@commoncode.com.au>
2014-03-18 11:26:50 +1100

    added coupons

---
+++ commercia/coupons/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from rea.models import Resource
+
+
+class Coupon(Resource):
+	'''
+	A Coupon is a Resource of sorts that is exchangeable under pre-determined
+	conditions as a sort of neo-currency.
+	'''
+	
+	code = models.CharField(
+		max_length=512)
