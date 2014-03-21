be06cc96e8ce39b408140666653bbb861fc52fe1
Daryl Antony <daryl@commoncode.com.au>
2014-03-21 11:14:36 +1100

    add the variant collection

--- commercia/products/collections.py
+++ commercia/products/collections.py
@@ -4,14 +4,20 @@ from cqrs.mongo import mongodb, DRFDocumentCollection
 
 
 class ProductDocumentCollection(DRFDocumentCollection):
-    """
-    A denormalized collection of `Products`
-    """
+
     name = 'product'
     model = Product
     serializer_class = 'commercia.products.serializers.ProductSerializer' # Polymorphic
     name = 'economica__products'
 
-    # XXX add signal listeners to models downcast from Product.
+
+class VariantDocumentCollection(DRFDocumentCollection):
+
+    name = 'variant'
+    model = Variant
+    serializer_class = 'commercia.products.serializers.VariantSerializer'
+    name = 'economica__products__variants'
+
 
 mongodb.register(ProductDocumentCollection())
+# mongodb.register(VariantDocumentCollection())
