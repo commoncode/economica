3d2b5b11f743a542708a66daac956b9e967e4b17
Daryl Antony <daryl@commoncode.com.au>
2014-03-21 11:19:16 +1100

    make sure coupons are there

--- commercia/coupons/models.py
+++ commercia/coupons/models.py
@@ -4,10 +4,10 @@ from rea.models import Resource
 
 
 class Coupon(Resource):
-	'''
-	A Coupon is a Resource of sorts that is exchangeable under pre-determined
-	conditions as a sort of neo-currency.
-	'''
-	
-	code = models.CharField(
-		max_length=512)
+    '''
+    A Coupon is a Resource of sorts that is exchangeable under pre-determined
+    conditions as a sort of neo-currency.
+    '''
+    
+    code = models.CharField(
+        max_length=512)
