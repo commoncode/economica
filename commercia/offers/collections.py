392b47e08fe247ecf5bba8eb23837d584c0e38b8
Daryl Antony <daryl@commoncode.com.au>
2014-04-02 01:49:51 +1100

    prune collections

--- commercia/offers/collections.py
+++ commercia/offers/collections.py
@@ -12,23 +12,4 @@ class OfferDocumentCollection(DRFDocumentCollection):
     name = 'economica__offers'
 
 
-class OfferResourceContractDocumentCollection(DRFDocumentCollection):
-
-    name = 'offerresourcecontract'
-    model = OfferResourceContract
-    serializer_class = 'commercia.offers.serializers.OfferResourceContractSerializer'
-    # parent_collection = OfferDocumentCollection
-    name = 'economica__offers__resourcecontracts'
-
-
-class OfferAspectDocumentCollection(DRFDocumentCollection):
-
-    name = 'offeraspect'
-    model = OfferAspect
-    serializer_class = 'commercia.offers.serializers.OfferAspectSerializer'
-    name = 'economica__offers__offeraspects'
-
-
 mongodb.register(OfferDocumentCollection())
-mongodb.register(OfferResourceContractDocumentCollection())
-mongodb.register(OfferAspectDocumentCollection())
