f274f0b25162d546cbd0b078df39a642e469a948
Daryl Antony <daryl@commoncode.com.au>
2014-04-15 10:30:25 +1000

    merge

--- commercia/quotes/serializers.py
+++ commercia/quotes/serializers.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 from cqrs.serializers import CQRSSerializer
 from commercia.offers.serializers import OfferSerializer
 
@@ -13,19 +15,29 @@ class QuoteItemSerializer(CQRSSerializer):
             'id',
             'quote',
             'offer',
-            'quantity'
+            'quantity',
+            'total'
         )
 
 
 class QuoteSerializer(CQRSSerializer):
     items = QuoteItemSerializer(many=True)
+    subtotal = serializers.FloatField(source='calculate_subtotal',
+        read_only=True)
+    shipping = serializers.FloatField(source='calculate_shipping',
+        read_only=True)
+    total = serializers.FloatField(source='calculate_total',
+        read_only=True)
 
     class Meta:
         model = Quote
         fields = (
-          'id',
-          'items',
-          'platform',
-          'providing_agent',
-          'recieving_agent'
+            'id',
+            'items',
+            'platform',
+            'providing_agent',
+            'recieving_agent',
+            'subtotal',
+            'shipping',
+            'total'
         )
