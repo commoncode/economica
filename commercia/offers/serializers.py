af395cc758287759a43cb3fc261c3fa3008fc2ff
Daryl Antony <daryl@commoncode.com.au>
2014-06-13 09:30:16 +1000

    Offer Aspect Serializer

--- commercia/offers/serializers.py
+++ commercia/offers/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 
 from cqrs.serializers import CQRSSerializer, CQRSPolymorphicSerializer
 from commercia.products.models import Category
-from rea_serializers.serializers import ResourceSerializer
+from rea_serializers.serializers import ContractSerializer, ResourceSerializer
+
 
 from . import models
 
 
 class CategoryArraySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Category
 
@@ -16,6 +18,7 @@ class CategoryArraySerializer(serializers.ModelSerializer):
 
 
 class CollectionArraySerializer(CQRSSerializer):
+
     class Meta:
         model = models.Collection
 
@@ -25,34 +28,59 @@ class CollectionArraySerializer(CQRSSerializer):
 
 class OfferResourceContractSerializer(CQRSSerializer):
 
+    contract = ContractSerializer()
     resource = ResourceSerializer()
 
     class Meta:
         model = models.OfferResourceContract
         fields = (
-            'id',
             'contract',
+            'id',
+            'quantity',
             'resource',
-            'quantity'
         )
 
 
+class OfferAspectSerializer(CQRSPolymorphicSerializer):
+
+    class Meta:
+        model = models.OfferAspect
+
+
 class OfferSerializer(CQRSSerializer):
-    collections = CollectionArraySerializer(source='collections_ids',
+
+    collections = CollectionArraySerializer(
+        source='collections_ids',
         read_only=True)
-    categories = CategoryArraySerializer(source='categories', read_only=True)
-    discount = serializers.IntegerField(source='discount', read_only=True)
-    price = serializers.FloatField(source='price', read_only=True)
-    quantity = serializers.IntegerField(source='quantity', read_only=True)
+
+    categories = CategoryArraySerializer(
+        source='categories',
+        read_only=True)
+
+    discount = serializers.IntegerField(
+        source='discount',
+        read_only=True)
+
+    price = serializers.FloatField(
+        source='price',
+        read_only=True)
+
+    quantity = serializers.IntegerField(
+        source='quantity',
+        read_only=True)
+
     resource_contracts = OfferResourceContractSerializer(many=True)
+
+    offer_aspects = OfferAspectSerializer(many=True)
 
     class Meta:
         model = models.Offer
         fields = (
-            'id',
-            'collections',
             'categories',
+            'collections',
             'discount',
+            'id',
+            'offer_aspects',
             'price',
             'quantity',
             'resource_contracts',
