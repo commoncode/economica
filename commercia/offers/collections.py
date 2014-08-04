3de2415d7f0e0e81cc6ac7a054839daeb1ba6748
Y3K <dboy.y3k@gmail.com>
2014-08-03 23:50:43 -0500

    Resource contract collections

--- commercia/offers/collections.py
+++ commercia/offers/collections.py
@@ -1,14 +1,22 @@
 from cqrs.mongo import mongodb
 from cqrs.collections import DRFDocumentCollection
 
-from .models import Collection, Offer
-from .serializers import CollectionSerializer, OfferSerializer
+from .models import Collection, Offer, OfferResourceContract
+from .serializers import (
+    CollectionSerializer, OfferResourceContractSerializer, OfferSerializer
+)
 
 
 class CollectionDocumentCollection(DRFDocumentCollection):
     model = Collection
     serializer_class = CollectionSerializer
     name = 'economica__collections'
+
+
+class OfferResourceContractDocumentCollection(DRFDocumentCollection):
+    model = OfferResourceContract
+    serializer_class = OfferResourceContractSerializer
+    name = 'economica__resource_contracts'
 
 
 class OfferDocumentCollection(DRFDocumentCollection):
@@ -18,4 +26,5 @@ class OfferDocumentCollection(DRFDocumentCollection):
 
 
 mongodb.register(CollectionDocumentCollection())
+mongodb.register(OfferResourceContractDocumentCollection())
 mongodb.register(OfferDocumentCollection())
